bc2751bf18d0f389b20321246c5229b73253a92e
svmk-khrustalev <hrustalev.aa@edu.spbstu.ru>
2022-11-17 21:55:42 +0300

    first commit

---
+++ src/test/java/com/example/lesson_1_3/Lesson13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lesson_1_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
